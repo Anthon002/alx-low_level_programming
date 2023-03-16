8ddfae977a557282a2339e63afadd6d1fcc14d65
Anthon002 <chineduanulugwo@gmail.com>
2023-03-16 12:14:34 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main function
+ * return 0
+ *
+ */
+int main(void)
+{
+	/*Printing using the puts function*/
+	puts("Programming is like building a multilingual puzzle \n");
+	return (0);
+}
