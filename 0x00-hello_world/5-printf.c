2f62c29a4695baf0767f88d17102701a0ea24811
Anthon002 <chineduanulugwo@gmail.com>
2023-03-16 12:27:01 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ * main - function
+ * Return value 0
+ */
+
+int main(void)
+{
+	printf("with proper gramma, but the outcom is a piece of art,\n");
+	return (0);
+}
