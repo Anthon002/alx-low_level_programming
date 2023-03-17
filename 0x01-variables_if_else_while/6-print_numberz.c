1a99eefeedfe159e25019af114ab0a473b3c55ed
Anthon002 <chineduanulugwo@gmail.com>
2023-03-17 12:49:28 -0700

    from 0 to 9

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Description: 'from 0 to 9'
- * Return: Always 0 (Success)
+ * main - Prints all single digit numbers of base 10 starting from 0,
+ *        only using putchar and without char variables.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	int a;
+	int num;
 
-	for (a = 48; a < 58; a++)
-	{
-		putchar(a);
-	};
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
 	putchar('\n');
+
+	return (0);
 }
