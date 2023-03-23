f0cc7b662140a575501f6ded1c8d6fdfbc74a9a5
Anthon002 <chineduanulugwo@gmail.com>
2023-03-23 14:17:46 -0700

    loop within a loop

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - loop in a loop
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	char a;
+	char b;
+
+	for (a = '0'; a <= '10'; a++)
+	{
+		for (b = '0'; b <= '14'; b++)
+		{
+			_putchar(b);
+		};
+		_putchar(b);
+	};
+}
