bfe5b7c68d0d37f1021733b0c7eb127805e93a17
Anthon002 <chineduanulugwo@gmail.com>
2023-03-23 13:37:57 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_numbers - main function
+ *
+ *Return: 0
+ */
+void print_numbers(void)
+{
+	for (int a = 0; a < 10; a++)
+	{
+		_putchar(a);
+	};
+	_putchar('\n');
+	return (0);
+}
