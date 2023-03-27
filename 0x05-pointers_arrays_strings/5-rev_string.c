8a1185115e69efcb0cff409ba6806bd6e9647451
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-03-27 20:40:58 +0100

    reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,14 +1,24 @@
-#include <stdio.h>
-#include <string.h>
 #include "main.h"
-
 /**
  * rev_string - main function
- * @s:parameter
- * Return: void;
+ * @s: parameter
+ * Return: 0 is success
  */
-
 void rev_string(char *s)
 {
-	*s = strrev(s);
+	int a, b, c;
+	char d;
+	
+	a = 0;
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	c = a - 1;
+	for (b = 0; c >= 0 && b < c; c--, b++)
+	{
+		d = s[b];
+		s[b] = s[c];
+		s[c] = d;
+	}
 }
