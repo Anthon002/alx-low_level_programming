e13f7cd014636ea6d8d75d00c7cc9617f16daf10
Anthon002 <chineduanulugwo@gmail.com>
2023-03-27 08:03:07 -0700

    Swapped values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main."
+
+/**
+ * swap_int- swapping values btw parameters
+ * @a: first parameter
+ * @b: second parameter
+ * Retur: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+	c = *a;
+	*a = *b;
+	*b = c;
+}
