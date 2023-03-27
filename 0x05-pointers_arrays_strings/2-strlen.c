2c51de634523b99e958f61c196f8213715f2a5e2
Anthon002 <chineduanulugwo@gmail.com>
2023-03-27 08:13:19 -0700

    sizeof()

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen- returns  lenght of strings
+ * @s: string parameter
+ * Return: value of sizeof(*s)
+ */
+
+int _strlen(char *s)
+{
+	int a = sizeof(*s);
+
+	return (a);
+}
