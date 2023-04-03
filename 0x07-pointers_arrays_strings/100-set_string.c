cee9308da21593669319bb9bec27eb392b266af6
Anthon002 <chineduanulugwo@gmail.com>
2023-04-03 15:48:14 -0700

    assigning pointer to string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - pointing pointer to a char
+ * @s: pointer pointer
+ * @to: pointer
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
