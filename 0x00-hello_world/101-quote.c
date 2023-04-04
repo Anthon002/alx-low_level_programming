94e35389077d98299330c6433707e7f5ff71257e
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-04-04 08:08:08 +0100

    Updated quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,13 @@
-/*
- * File: 101-quote.c
- * Auth: Brennan D Baraban
- */
-
 #include <unistd.h>
 
 /**
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *                followed by a new line, to standard error.
+ * main - main function
  *
- * Return: Always 1.
+ * Return: 1
  */
 int main(void)
 {
-	write(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      59);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 
 	return (1);
 }
