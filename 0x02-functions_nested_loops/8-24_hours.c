11b6f8e79bdb5927d81552208c67a80ed150cb5a
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-04-04 10:21:30 +0100

    hours of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,23 +1,26 @@
 #include "main.h"
 
 /**
- * jack_bauer - Prints every minute of the day of
- *              Jack Bauer, starting from 00:00 to 23:59.
+ * jack_bauer - fucntion to print every minute of the day
  */
 void jack_bauer(void)
 {
-	int hour, minute;
+    int hour, minute;
 
-	for (hour = 0; hour <= 23; hour++)
-	{
-		for (minute = 0; minute <= 59; minute++)
-		{
-			_putchar((hour / 10) + '0');
-			_putchar((hour % 10) + '0');
-			_putchar(':');
-			_putchar((minute / 10) + '0');
-			_putchar((minute % 10) + '0');
-			_putchar('\n');
-		}
-	}
+    for (hour = 0; hour < 24; hour++) {
+        for (minute = 0; minute < 60; minute++) {
+            int tens_hour = hour / 10;
+            int ones_hour = hour % 10;
+            int tens_minute = minute / 10;
+            int ones_minute = minute % 10;
+
+            _putchar(tens_hour + '0');
+            _putchar(ones_hour + '0');
+            _putchar(':');
+            _putchar(tens_minute + '0');
+            _putchar(ones_minute + '0');
+            _putchar('\n');
+        }
+    }
 }
+
