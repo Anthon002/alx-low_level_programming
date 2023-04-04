8f1ae0e2fe444e0d31f15fa9e4fa59802e10259d
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-04-04 10:10:30 +0100

    absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
 /**
- * _abs - Computes the absolute value of an integer.
- * @n: The integer to be computed.
+ * _abs - absolute value.
+ * @n: integer .
  *
- * Return: The absolute value of the integer.
+ * Return: abs value.
  */
 int _abs(int n)
 {
 	if (n >= 0)
+	{
 		return (n);
+	}
 	else
+	{
 		return (-n);
+	};
 }
