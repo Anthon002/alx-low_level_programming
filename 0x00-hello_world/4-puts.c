4575970fb0f880fa76cb000d271ca2c032c9d90d
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-04-04 08:10:09 +0100

    Updated Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - function
+ * main - main function
  * Return: 0
  *
  */
 int main(void)
 {
-	/*Printing using the puts function*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
