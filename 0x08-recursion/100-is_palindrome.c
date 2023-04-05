23a8a8b6dd4d635578d8a793d67c5046323f3fac
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-04-05 23:20:37 +0100

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,30 +1,44 @@
 #include "main.h"
-#include <string.h>
-#include <stdio.h>
 
 /**
- * is_palindrome - checks to see if a word is a palindrome
- * @s: parameter
- * Return: 0 or 1
+ * string_length - Calculates the length of the string
+ * @str: Pointer to the string
+ * Return: The length of the string
+ * check_palindrome - Check if a string is palindrome or not
+ * @str: Pointer to the string
+ * @left: The left index of the string
+ * @right: The right index of the string
+ * Return: 1 if the string is palindrome, 0 otherwise
+ * is_palindrome - Check if a string is palindrome or not
+ * @s: Pointer to the string
+ * Return: 1 if the string is palindrome, 0 otherwise
+ * 
  */
-
-int is_palindrome(char *s)
+int string_length(char *s)
 {
-	if (*s == '\0' || *(s + 1) == '\0')
+	if (*str == '\0')
 	{
 		return (0);
 	}
-	else
-	{
-		if (*s == *(s + (strlen(s) - 1)))
-		{
-			*(s + (strlen(s) - 1)) = '\0';
-			return (is_palindrome(s + 1));
-		}
-		else
-		{
-			return (1);
-	}
-	}
+	return (1 + string_length(str + 1));
 }
 
+
+int check_palindrome(char *str, int left, int right)
+{
+	if (left >= right)
+	{
+		return (1);
+	}
+	if (str[left] != str[right])
+	{
+		return (0);
+	}
+	return (check_palindrome(str, left + 1, right - 1));
+}
+
+int is_palindrome(char *s)
+{
+	int len = string_length(s) - 1;
+	return (check_palindrome(s, 0, len));
+}
