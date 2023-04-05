82abb0c52971cdd13a05f6d0e1ee8cf2a08d9d63
Anthon002 <chineduanulugwo@gmail.com>
2023-04-05 15:27:48 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,15 +4,6 @@
  * string_length - Calculates the length of the string
  * @str: Pointer to the string
  * Return: The length of the string
- * check_palindrome - Check if a string is palindrome or not
- * @str: Pointer to the string
- * @left: The left index of the string
- * @right: The right index of the string
- * Return: 1 if the string is palindrome, 0 otherwise
- * is_palindrome - Check if a string is palindrome or not
- * @s: Pointer to the string
- * Return: 1 if the string is palindrome, 0 otherwise
- * 
  */
 int string_length(char *str)
 {
@@ -23,6 +14,13 @@ int string_length(char *str)
 	return (1 + string_length(str + 1));
 }
 
+/**
+ * check_palindrome - Check if a string is palindrome or not
+ * @str: Pointer to the string
+ * @left: The left index of the string
+ * @right: The right index of the string
+ * Return: 1 if the string is palindrome, 0 otherwise
+ */
 
 int check_palindrome(char *str, int left, int right)
 {
@@ -37,8 +35,15 @@ int check_palindrome(char *str, int left, int right)
 	return (check_palindrome(str, left + 1, right - 1));
 }
 
+/**
+ * is_palindrome - Check if a string is palindrome or not
+ * @s: Pointer to the string
+ * Return: 1 if the string is palindrome, 0 otherwise
+ */
+
 int is_palindrome(char *s)
 {
 	int len = string_length(s) - 1;
+
 	return (check_palindrome(s, 0, len));
 }
