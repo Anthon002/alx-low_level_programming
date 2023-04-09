bbb67adc36b49ea99f2077b4933085d553dfd598
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-04-09 06:16:51 +0100

    multiply arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 	{
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	}
-	else
+	else if (argc == 0)
 	{
 		printf("Error: please provide two integer arguments\n");
 	}
