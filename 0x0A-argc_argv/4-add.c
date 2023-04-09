4de21859c083135e51c7d9b730ee84ed7ab6d809
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-04-09 07:01:56 +0100

    add arguments

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,13 +5,12 @@
  * main - adding positive numbers.
  * @argc: argument count
  * @argv: arguments
- *
  * Return: 0 on success, 1 on failure
  */
 int main(int argc, char *argv[])
 {
-	int index, num, sum = 0;
 	char *endptr;
+	int index = 1, num, sum = 0;
 
 	if (argc < 2)
 	{
@@ -19,19 +18,20 @@ int main(int argc, char *argv[])
 		return (0);
 	}
 
-	for (index = 1; argv[index] != NULL; index++)
+	while (argv[index] != NULL)
 	{
 		num = strtol(argv[index], &endptr, 10);
 		if (*endptr != '\0')
-	{
-		    printf("Error\n");
-		    return (1);
-	}
-	sum += num;
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += num;
+		index++;
 	}
 	printf("%d\n", sum);
 
 	return (0);
-	}
+}
 
 
