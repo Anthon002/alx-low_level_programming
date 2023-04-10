607315db389c9a6ffa7174f9120f4086d78f5ebb
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-04-10 19:11:19 +0100

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - calculating change
+ *@arc: int parameter
+ *@argv: char parameter
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+  int amount, coinsUsed = 0;
+
+  if (argc != 2) {
+    printf("Usage: %s amount\n", argv[0]);
+    return 1;
+  }
+
+  amount = atoi(argv[1]);
+
+  for (; amount > 0; coinsUsed++) {
+    if (amount >= 25) {
+      amount -= 25;
+    } else if (amount >= 10) {
+      amount -= 10;
+    } else if (amount >= 5) {
+      amount -= 5;
+    } else if (amount >= 2) {
+      amount -= 2;
+    } else if (amount >= 1) {
+      amount -= 1;
+    }
+  }
+
+  printf("%d\n", coinsUsed);
+  return 0;
+}
 
