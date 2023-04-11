d4f14227117e88405cd5b9b5afa278ef4d29e611
Anthon002 <chineduanulugwo@gmail.com>
2023-04-11 12:26:16 -0700

    reallocating

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,17 +14,17 @@ char *_strdup(char *str)
 	int i = 0;
 	int j = 0;
 
-	while (str[j] != '\0')
-	{
-		j++;
-	}
-
 	if (str == NULL)
 	{
 		return (NULL);
 	}
 
-	new = (char *) malloc(sizeof(char) * j);
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+	new = (char *) malloc(sizeof(char) * (j + 1));
 
 	if (new == NULL)
 	{
@@ -34,9 +34,10 @@ char *_strdup(char *str)
 	{
 		while (i < j)
 		{
-			i++;
 			new[i] = str[i];
+			i++;
 		}
 		return (new);
 	}
+	free(new);
 }
