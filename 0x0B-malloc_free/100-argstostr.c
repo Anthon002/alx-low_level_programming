6c56ada16a5379e212834a8c96b0c0a42e4cdb86
Anthon002 <chineduanulugwo@gmail.com>
2023-04-11 14:58:00 -0700

    concat arguments

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,37 +3,39 @@
 #include "main.h"
 
 /**
- * argstostr - concatenates all the arguments of a program into a single string
- * @ac: number of arguments
- * @av: array of strings containing the arguments
- * Return: a pointer to a new string, or NULL if it fails
+ * argstostr - concatenate argument
+ * @ac: argument count 
+ * @av: argument array
+ * Return: a pointer or Null
  */
 
 char *argstostr(int ac, char **av)
 {
-    char *result;
-    int i, j, total_len = 0;
+	char *result;
+	int i, j, total_len = 0;
 
-    if (ac == 0 || av == NULL)
-        return NULL;
+	if (ac == 0 || av == NULL)
+	{
+		return NULL;
+	}
 
-    for (i = 0; i < ac; i++)
-    {
-        total_len += strlen(av[i]);
-        total_len++;
-    }
+	for (i = 0; i < ac; i++)
+	{
+		total_len += strlen(av[i]);
+		total_len++;
+	}
 
-    result = (char *) malloc(sizeof(char) * (total_len + 1));
-    if (result == NULL)
-        return NULL;
-
-    for (i = 0, j = 0; i < ac; i++)
-    {
-        strcpy(result + j, av[i]);
-        j += strlen(av[i]);
-        result[j++] = '\n'; 
-    }
-    result[total_len] = '\0';
-
-    return result;
+	result = (char *) malloc(sizeof(char) * (total_len + 1));
+	if (result == NULL)
+	{
+		return NULL;
+	}
+	for (i = 0, j = 0; i < ac; i++)
+	{
+		strcpy(result + j, av[i]);
+		j += strlen(av[i]);
+		result[j++] = '\n'; 
+	}
+	result[total_len] = '\0';
+	return result;
 }
