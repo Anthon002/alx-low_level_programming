318f3fe9d8cc7147a287c2b16c282a351acda6fb
Anthon002 <chineduanulugwo@gmail.com>
2023-04-11 14:35:31 -0700

    concat arguments

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * concat_args - concatenates all the arguments
+ * @ac: number of arguments
+ * @av: array of strings containing the arguments
+ * Return: a pointer to a new string, or NULL if it fails
+ */
+
+char *concat_args(int ac, char **av)
+{
+	char *result;
+	int i, j, total_len = 0;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		total_len += strlen(av[i]);
+		if (i != ac - 1)
+		{
+			total_len++;
+		}
+	}
+
+	result = malloc(sizeof(char) * (total_len + 1));
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0, j = 0; i < ac; i++)
+	{
+		strcpy(result + j, av[i]);
+		j += strlen(av[i]);
+		if (i != ac - 1)
+		{
+			result[j++] = ' ';
+		}
+	}
+	result[total_len] = '\0';
+	return (result);
+}
