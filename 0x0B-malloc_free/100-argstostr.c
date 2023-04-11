40bebeaf1a5e4f27a00b5753969061306f12489e
Anthon002 <chineduanulugwo@gmail.com>
2023-04-11 14:45:37 -0700

    concat arguments

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -21,10 +21,6 @@ char *argstostr(int ac, char **av)
 	for (i = 0; i < ac; i++)
 	{
 		total_len += strlen(av[i]);
-		if (i != ac - 1)
-		{
-			total_len++;
-		}
 	}
 
 	result = malloc(sizeof(char) * (total_len + 1));
@@ -37,10 +33,7 @@ char *argstostr(int ac, char **av)
 	{
 		strcpy(result + j, av[i]);
 		j += strlen(av[i]);
-		if (i != ac - 1)
-		{
-			result[j++] = ' ';
-		}
+		result[j++] = '\n';
 	}
 	result[total_len] = '\0';
 	return (result);
