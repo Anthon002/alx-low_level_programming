609cace08c8233a30f6ea249fd1cd606cafed9d4
Anthon002 <chineduanulugwo@gmail.com>
2023-04-13 14:16:32 -0700

    allocated memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memeory using mallo
+ * @b: int parameter
+ * Return: 98 or pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *new = (int *) malloc(sizeof(int) * b);
+
+	if (new == NULL)
+	{
+		exit (98);
+	}
+	else
+	{
+		return (new);
+	}
+}
