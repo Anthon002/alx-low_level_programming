8dd94f63c30ffd8e4d21870772e672df79eeaf4f
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-04-13 23:15:16 +0100

    Concatenate strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,67 +1,71 @@
 #include "main.h"
 
 /**
- * string_nconcat - a function that concatenates two strings.
- *
- * @s1: first char
- * @s2: secound char
- * @n: unsigned int
- *
- * Return: If the function fails, it should return NULL
+ * string_nconcat - concatenates two strings.
+ * @s1: string 1
+ * @s2: string 2
+ * @n: space
+ * Return: pointer or NULL
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, j = 0, k = 0;
-	char *s;
+    unsigned int i = 0, j = 0, k = 0;
+    char *concat;
 
-	if (s1 == NULL)
-	{
-		i = 0;
-	}
-	else
-	{
-		while (s1[i])
-		{
-			i++;
-		}
-	}
+    if (s1 == NULL)
+    {
+        i = 0;
+    }
+    else
+    {
+        while (s1[i] != '\0')
+        {
+            i++;
+        }
+    }
+    if (s2 == NULL)
+    {
+        j = 0;
+    }
+    else
+    {
+        while (s2[j] != '\0')
+        {
+            j++;
+        }
+    }
 
-	if (s2 == NULL)
-	{
-		j = 0;
-	}
-	else
-	{
-		while (s2[j])
-		{
-			j++;
-		}
-	}
+    if (j > n)
+    {
+        j = n;
+    }
+    concat = malloc(sizeof(char) * (i + j + 1));
 
-	if (j > n)
-	{
-		j = n;
-	}
+    if (concat == NULL)
+    {
+        return (NULL);
+    }
+    else
+    {
+        i = 0;
 
-	s = malloc(sizeof(char) * (i + j + 1));
-	if (s == NULL)
-	{
-		return (NULL);
-	}
+        while (s1[i] != '\0')
+        {
+            concat[k] = s1[i];
+            i++;
+            k++;
+        }
 
-	while (k < i)
-	{
-		s[k] = s1[k];
-		k++;
-	}
+        j = 0;
 
-	while (k < i + j)
-	{
-		s[k] = s2[k - i];
-		k++;
-	}
+        while (j < n && s2[j] != '\0')
+        {
+            concat[k] = s2[j];
+            j++;
+            k++;
+        }
 
-	s[k] = '\0';
-
-	return (s);
+        concat[k] = '\0';
+        return (concat);
+    }
 }
