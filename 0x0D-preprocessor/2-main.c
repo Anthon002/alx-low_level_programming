a8be50d18a8af673ea16a9803f015127a081f6da
Anthon002 <chineduanulugwo@gmail.com>
2023-04-17 11:43:40 -0700

    printfilename

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints out the filename
+ * @argc: argument count 
+ * @argv: arguments
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s",argv[0]);
+	return (0);
+}
