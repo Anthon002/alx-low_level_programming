7de21372028e9b685b64dd966e4f55d771f6378d
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-04-17 22:55:10 +0100

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,12 +15,13 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+typedef struct dog dog_t;
+
 void print_dog(struct dog *d);
 
-typedef struct dog dog_type;
+void free_dog(dog_t *d);
 
-dog_type *new_dog(char *name, float age, char *owner);
-
-void dogFree(dog_type *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
