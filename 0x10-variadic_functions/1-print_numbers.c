f6bcad943fb554787aade091c98ab2240e081293
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-04-20 18:41:45 +0100

    print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function to print no.s with newlines
+ * @separator: separator 
+ * @n: number of arguments
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+  va_list args;
+	unsigned int i;
+	int num;
+
+	va_start(args, n);
+
+	if (separator == NULL)
+		separator = "";
+
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(args, int);
+		printf("%d", num);
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
