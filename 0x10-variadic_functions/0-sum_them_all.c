52fcad518e962de2c81e8d8d86b62a8bd75a1d14
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-04-20 18:34:54 +0100

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,14 +14,14 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(args, n);
 
-	if (n == 0) {
+	if (n == 0)
+	{
 		va_end(args);
 		return (0);
 	}
-
-	for (i = 0; i < n; i++) {
+	
+	for (i = 0; i < n; i++)
 		sum += va_arg(args, int);
-	}
 
 	va_end(args);
 
