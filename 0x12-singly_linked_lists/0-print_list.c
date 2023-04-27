25c8972946c7c62cc06c082b42f118031f9f1b23
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-04-27 17:17:09 +0100

    print elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,10 @@ size_t print_list(const list_t *head)
 {
 	size_t count = 0;
 	const list_t *current = head;
-
+	if (current == NULL)
+	{
+		printf("[0] (nil)");
+	};
 	while (current != NULL)
 	{
 		printf("[%u] %s\n", current->len, current->str);
