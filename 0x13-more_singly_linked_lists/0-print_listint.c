adc9628c0b3c6a8447642080ea8b1891ae24285a
Anthon002 <chineduanulugwo@gmail.com>
2023-05-01 08:09:36 -0700

    print elements of a list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,8 +13,8 @@ size_t print_listint(const listint_t *h)
 
 	while (current != NULL)
 	{
-		printf("%d", current -> n);
-		current = current -> next;
+		printf("%d\n", current->n);
+		current = current->next;
 		count++;
 	};
 
