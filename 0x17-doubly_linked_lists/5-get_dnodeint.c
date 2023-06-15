cc25e70a2b2aaf47a06adfa30f36d63adc23d98a
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-06-15 21:45:27 +0100

    fixed betty

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	while (crnt != NULL)
 	{
 		if (ct == index)
-			return crnt;
+			return (crnt);
 		crnt = crnt->next;
 		ct++;
 	}
