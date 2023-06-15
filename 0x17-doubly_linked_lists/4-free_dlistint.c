3351c7d454b5bde3d562649c0944f514170c6c8b
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-06-15 21:36:51 +0100

    free dlistint

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_dlistint - frees list
+ * @head: points to the start of a linked list
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *crnt, *nxt;
+
+	crnt = head;
+	while (crnt != NULL)
+	{
+		nxt = crnt->nxt;
+		free(crnt);
+		crnt = nxt;
+	}
+}
