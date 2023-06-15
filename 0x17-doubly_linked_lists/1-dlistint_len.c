1a0ec70591a27303fe7ed798e83388a75254184e
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-06-15 21:18:42 +0100

    number of nodes

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - number of nodes
+ * @h: start pointer
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	if (h == NULL)
+		return 0;
+	return (dlistint_len(h->next) + 1);
+}
