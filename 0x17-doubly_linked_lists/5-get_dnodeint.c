d29a4d8b322af771cb07990dc62fd680d770565c
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-06-15 21:47:32 +0100

    fixed betty

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,11 +1,10 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index - finds a specific node of a linked list
- * @head: pointer to the beginning of the list
- * @index: index of the node to retrieve
- *
- * Return: pointer to the indexed node, or NULL on failure
+ * get_dnodeint_at_index - finds a  node from a linked list
+ * @head: pointer
+ * @index: index
+ * Return: pointer
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
