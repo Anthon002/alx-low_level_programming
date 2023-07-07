f70c1d263553bd33f6b1746fa3d74f872e365f78
Anthon002 <chineduanulugwo@gmail.com>
2023-07-07 00:43:23 -0700

    task 01

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,14 +1,14 @@
 #include "hash_tables.h"
 /**
  * hash_djb2 - the daniel j bernstein hash function
- * str: the key
+ * @str:the key
  * Return: the unique index
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
 	int c;
-	
+
 	hash = 5381;
 	while ((c = *str++))
 	{
